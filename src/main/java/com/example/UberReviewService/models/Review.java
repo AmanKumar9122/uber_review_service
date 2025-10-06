d62b87d7d47d22423ce272dd26b9b535acbeb5d4
AmanKumar9122 <amanraj15112003@gmail.com>
2025-10-06 14:40:06 +0530

    added first basic model

---
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -0,0 +1,13 @@
+package com.example.UberReviewService.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+            // @Id - used for declare primary key
+    Long id;
+
+
+}
